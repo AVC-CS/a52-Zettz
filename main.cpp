185ae94d8bf44a996961d3040634f1652fa2ba91
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-23 20:59:59 +0000

    added a52 code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    // TODO
+    int num, max_val;
+    cin >> num; // Read the first number
+    max_val = num; // Initialize max_val with the first number
 
+    for(int i=1;i<10;i++){ // Loop for the remaining 9 numbers
+      cin >> num;
+      if ( num > max_val )
+        max_val = num;
+    }
+
+    cout << " Max " << max_val << endl;
     return 0;
 }
